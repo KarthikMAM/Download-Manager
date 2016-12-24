1cac650f0509e282ba2aef2787963f2a7405d8f6
Karthik M A M <karthik_m_a_m@outlook.com>
2016-12-24 10:41:48 +0530

    implement download features and ui

---
+++ DownloadManager/MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using DownloadHelper;
+using Microsoft.Win32;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DownloadManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Downloader downloader;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //if text is in clipboard copy it.
+            //if (Clipboard.ContainsText()) txtSource.Text = Clipboard.GetText();
+        }
+
+        /// <summary>
+        /// upon closing the window abort the download
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (downloader != null) downloader.Abort();
+        }
+
+        /// <summary>
+        /// responses to various button click scenarios
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //get the sender button
+            Button btSender = (Button)sender;
+
+            //for controller use content and others their name
+            switch (btSender != btController ? btSender.Name : btSender.Content.ToString())
+            {
+                case "btTarget":
+                    try
+                    {
+                        //find the file name from the url and use it
+                        SaveFileDialog selectTargetDialog = new SaveFileDialog();
+                        selectTargetDialog.FileName = Downloader.FindFileName(txtSource.Text);
+                        selectTargetDialog.Title = "Select a file to save download";
+                        if (selectTargetDialog.ShowDialog() == true)
+                        {
+                            txtTarget.Text = selectTargetDialog.FileName;
+                        }
+                    }
+                    catch (Exception ex) { MessageBox.Show(ex.Message, "Error"); }
+                    break;
+                case "Start":
+                    //disable items on window to prevent inconsistency
+                    if (txtSource.Text.Length != 0 && txtTarget.Text.Length != 0)
+                    {
+                        txtSource.IsEnabled = txtTarget.IsEnabled = btTarget.IsEnabled = false;
+                        btController.IsEnabled = false;
+
+                        //create a new downloader object
+                        downloader = new Downloader(txtSource.Text, txtTarget.Text);
+                        downloader.DwnlTracker.Tracker.Tick += Tracker_Tick;
+                        downloader.Start();
+                    }
+                    else { MessageBox.Show("Need Download source and target path", "Incomplete Data"); }
+                    break;
+                case "Pause":
+                    btController.IsEnabled = false;
+
+                    downloader.Abort();
+                    break;
+                case "Resume":
+                    btController.IsEnabled = false;
+
+                    downloader.Start();
+                    break;
+
+            }
+        }
+
+        private void Tracker_Tick(object sender, EventArgs e)
+        {
+            switch (downloader.State)
+            {
+                case Downloader.DownloadState.Download:
+                    btController.Content = "Pause";
+                    btController.IsEnabled = true;
+
+                    //update progress of the download
+                    txtSize.Content = String.Format("{0:f3} MB", (double)downloader.DwnlSize / Downloader.MB);
+                    txtComplete.Content = String.Format("{0:f3} MB", (double)downloader.DwnlTracker.DwnlCompleted / Downloader.MB);
+                    txtSpeed.Content = String.Format("{0:f3} MBps", (double)downloader.DwnlTracker.DwnlSpeed / Downloader.MB);
+
+                    break;
+                case Downloader.DownloadState.Abort:
+                    txtSpeed.Content = "0.000 MBps";
+                    break;
+                case Downloader.DownloadState.Idle:
+                    btController.Content = "Resume";
+                    btController.IsEnabled = true;
+                    break;
+                case Downloader.DownloadState.Append:
+                case Downloader.DownloadState.Finish:
+                    btController.IsEnabled = false;
+
+                    //update progress of the download
+                    txtSize.Content = String.Format("{0:f3} MB", (double)downloader.DwnlSize / Downloader.MB);
+                    txtComplete.Content = String.Format("{0:f3} MB", (double)downloader.DwnlTracker.DwnlCompleted / Downloader.MB);
+                    txtSpeed.Content = String.Format("{0:f3} MBps", (double)downloader.DwnlTracker.DwnlSpeed / Downloader.MB);
+                    break;
+            }
+
+            //update the progress bar value and its message
+            txtProgress.Content = downloader.DwnlTracker.DwnlProgressMsg;
+            barProgress.Value = downloader.DwnlTracker.DwnlProgress;
+        }
+    }
+}
---
+++ Downloader/ChunkDownloader.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DownloadHelper
+{
+    public class ChunkDownloader
+    {
+        //download thread
+        private Thread dwnlThread;
+        public Exception DwnlException { private set; get; }
+        private const int BUFFER_SIZE = 8192;
+        private byte[] buffer = new byte[BUFFER_SIZE];
+
+        //chunk data
+        private string chunkSource;
+        private string chunkTarget;
+        private long chunkStart;
+        private long chunkEnd;
+
+        /// <summary>
+        /// initializes the download thread for the given chunk
+        /// </summary>
+        /// <param name="chunkSource">url to download from</param>
+        /// <param name="chunkTarget">url to save download</param>
+        /// <param name="chunkStart">starting position of the chunk</param>
+        /// <param name="chunkEnd">ending position of the chunk</param>
+        public ChunkDownloader(string chunkSource, string chunkTarget, long chunkStart, long chunkEnd)
+        {
+            //set the parameters
+            this.chunkSource = chunkSource;
+            this.chunkTarget = chunkTarget;
+            this.chunkStart = chunkStart;
+            this.chunkEnd = chunkEnd;
+
+            //create a new thread to download the chunk
+            //and abort if the dwnl request is still open
+            dwnlThread = new Thread(new ThreadStart(Download));
+        }
+
+        /// <summary>
+        /// start the thread if not running
+        /// </summary>
+        public void Start()
+        {
+            dwnlThread.Start();
+        }
+
+        /// <summary>
+        /// waits till the thread finishes running
+        /// </summary>
+        public void Join()
+        {
+            if (dwnlThread.IsAlive)
+            {
+                dwnlThread.Join();
+            }
+        }
+
+        /// <summary>
+        /// abort thread the thread
+        /// </summary>
+        public void Abort()
+        {
+            if (dwnlThread.IsAlive)
+            {
+                dwnlThread.Abort();
+                dwnlThread.Join();
+            }
+        }
+
+        /// <summary>
+        /// download logic
+        /// </summary>
+        public void Download()
+        {
+            //adjust the download range and the completed part
+            chunkStart += File.Exists(chunkTarget) ? new FileInfo(chunkTarget).Length : 0;
+
+            //check if there is a need to download
+            if (chunkStart < chunkEnd)
+            {
+                //prepare the download request
+                HttpWebRequest dwnlReq = WebRequest.CreateHttp(chunkSource);
+                dwnlReq.AllowAutoRedirect = true;
+                dwnlReq.AddRange(chunkStart, chunkEnd);
+                dwnlReq.ServicePoint.ConnectionLimit = 100;
+                dwnlReq.ServicePoint.Expect100Continue = false;
+
+                try
+                {
+                    using (HttpWebResponse dwnlRes = (HttpWebResponse)dwnlReq.GetResponse())
+                    using (Stream dwnlSource = dwnlRes.GetResponseStream())
+                    using (FileStream dwnlTarget = new FileStream(chunkTarget, FileMode.Append, FileAccess.Write))
+                    {
+                        int bufferdSize;
+                        do
+                        {
+                            //read the download response async
+                            //in the mean time flush the writable data and wait for result
+                            //write the new buffered data to the target stream
+                            Task<int> bufferReader = dwnlSource.ReadAsync(buffer, 0, BUFFER_SIZE);
+                            dwnlTarget.Flush();
+                            bufferReader.Wait();
+                            bufferdSize = bufferReader.Result;
+                            dwnlTarget.Write(buffer, 0, bufferdSize);
+                        } while (bufferdSize > 0);
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    DwnlException = e;
+                }
+                finally
+                {
+                    dwnlReq.Abort();
+                }
+            }
+        }
+    }
+}
