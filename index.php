58cb59d86eb61faa58e9c7a057a68268083ce9c4
Karthik M A M <karthik_m_a_m@outlook.com>
2016-12-26 14:18:31 +0530

    change file server logic to add hostinger

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <?php
 	extract($_GET);
-
+	
 	//get the length from the url
 	//and print the length
 	$length = get_headers($url, 1)['Content-Length'];
 	if(gettype($length) == "array") {
 		$length = end($length);
-	}
+	}	
 	print($length);
-	syslog(LOG_INFO, $url . $length);
-?>
+	
+    //if server is hostinger log the requests
+    if ($_SERVER['SERVER_NAME'] == 'download-manager.esy.es') {
+
+        //mysql parameters
+        $server = "localhost";
+        $user = "u466721340_user";
+        $password = "123456";
+        $database = "u466721340_data";
+        $query = "insert into url(url, size) values('" . $url . "'," . $length . ");";
+        
+        //sql connection and query
+        $conn = new mysqli($server, $user, $password, $database) or die();
+        $conn->query($query);
+    }
+
+?>		
